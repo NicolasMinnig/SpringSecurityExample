441240c864ff4618fb610193fbe6d7a83a67ea48
lucien.moor <lucien.moor@he-arc.ch>
2020-01-14 13:34:08 +0100

    updated for jdk11

--- SpringTestDemo/src/main/java/com/example/demo/WebSecurityConfig.java
+++ SpringTestDemo/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -1,10 +1,5 @@
 package com.example.demo;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +11,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
--- SpringTestDemo/src/main/java/com/example/demo/controller/HomeController.java
+++ SpringTestDemo/src/main/java/com/example/demo/controller/HomeController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.models.User;
-import com.example.demo.repository.UserRepository;
 import com.example.demo.services.UserService;
 
 @Controller
